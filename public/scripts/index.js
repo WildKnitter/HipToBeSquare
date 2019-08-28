f64107a90d81dd055ddd52937c059461355c1a09
Pamela Belknap <pbelknap@cox.net>
2019-08-28 12:23:32 -0400

    index page and js script

---
+++ public/scripts/index.js
@@ -0,0 +1,41 @@
+"use strict";
+// This script contains code to dynamically create dropdown information on different organizations from a JSON file.
+// Author:  Pam Belknap
+
+/*
+Key for understanding the fields in the JSON file:
+leagues
+leagues.length
+leagues[i].Name
+leagues[i].Code
+leagues[i].Description
+
+/api/leagues
+/api/teams
+/api/teams/:id
+/api/teams/byleague/:id
+/api/teams/:teamid/members/:memberid
+/api/teams/:id/members
+*/
+
+$(function() {
+    getLeaguesList();
+});
+
+// getLeagues Function to fill the unordered list for LEAGUES on the Index Page.
+function getLeaguesList() {
+    let objs;
+    //Starts the communication to the server
+    $.getJSON(
+        "/api/leagues",
+        //This function doesn't necessarily run instantaneously
+        function(data) {
+            objs = data;
+            $("#leagueList").empty();
+            for (let i = 0; i < objs.length; i++) {
+                let markupList = "<ul><li>" + objs[i].Name + "</li></ul>";
+                $("#leagueList").append(markupList);
+            } // end of for
+        } // end of CALLBACK function
+    ); // end of call to $.getJSON     
+} // end of getLeaguesList function
