f98b313a04a76975e5a74200bd1e3f179f86af30
Pamela Belknap <pbelknap@cox.net>
2019-09-03 17:39:30 -0400

    Updated Patterns Page

---
+++ public/scripts/patterns.js
@@ -0,0 +1,28 @@
+"use strict";
+//This scripts contains code to alternate patterns for either knitting or crocheting.
+//Author:  Pam Belknap
+
+$(function() {
+
+        $("#knitPatterns").hide();
+        $("#crochetPatterns").hide();
+        $("#btnKnitting").on("click", showKnittingPatterns);
+        $("#btnCrocheting").on("click", showCrochetingPatterns);
+        $("#btnReset").on("click", resetPage);
+
+        function showKnittingPatterns() {
+            $("#knitPatterns").show();
+            $("#crochetPatterns").hide();
+        }
+
+        function showCrochetingPatterns() {
+            $("#crochetPatterns").show();
+            $("#knitPatterns").hide();
+        }
+
+        function resetPage() {
+            $("#knitPatterns").hide();
+            $("#crochetPatterns").hide();
+        }
+
+    }) // end of READY EVENT HANDLER
